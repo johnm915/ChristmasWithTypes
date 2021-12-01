2cac503994b93d76bdcc7364739654aff0e588cf
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2021-12-01 11:02:54 -0600

    finished excersise

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -9,16 +9,18 @@ namespace ChristmasWithTypes
             var xmas = new Christmas();
 
             var xmasDay = Christmas.Day.Thursday;
+           
+        
 
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
-
+        //TODO set Santa's name to Kris Kringle
+        xmas.Santa = "kris Kringle";   
+             
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{"Xbox", "Nintendo Stwich", "Socks" }; 
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
-
+            xmas.TreeHeight = 10;
+           
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
             Console.WriteLine("Here are the presents we would like:");
